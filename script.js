10e82246807a877af4a0bd0ead9a6d3819f7c9bb
nillenia <alpayuerkuendag@gmail.com>
2025-05-28 14:39:55 +0200

    Added Seconds to the timer

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function updateTime() {
     const now = new Date();
     const h = now.getHours().toString().padStart(2, '0');
     const m = now.getMinutes().toString().padStart(2, '0');
-    document.getElementById('current-time').textContent = `${h}:${m}`;
+    const s = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('current-time').textContent = `${h}:${m}:${s}`;
 }
 setInterval(updateTime, 1000);
 updateTime();
